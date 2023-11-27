767fd7b67398f908b8ea4728f88909a5e49f8d0d
Giacomo Arienti <arientigiacomo2003@gmail.com>
2023-11-27 10:50:39 +0100

    exercise 101

--- 101-lambda-utilities-for-lists-and-maps/src/main/java/it/unibo/oop/lab/lambda/LambdaUtilities.java
+++ 101-lambda-utilities-for-lists-and-maps/src/main/java/it/unibo/oop/lab/lambda/LambdaUtilities.java
@@ -2,6 +2,7 @@ package it.unibo.oop.lab.lambda;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -58,10 +59,9 @@ public final class LambdaUtilities {
      *         otherwise.
      */
     public static <T> List<Optional<T>> optFilter(final List<T> list, final Predicate<T> pre) {
-        /*
-         * Suggestion: consider Optional.filter
-         */
-        return null;
+        final List<Optional<T>> listOfOptionals = new ArrayList<>(list.size());
+        list.forEach(el -> listOfOptionals.add(Optional.of(el).filter(pre)));
+        return listOfOptionals;
     }
 
     /**
@@ -77,10 +77,16 @@ public final class LambdaUtilities {
      *         based on the mapping done by the function
      */
     public static <R, T> Map<R, Set<T>> group(final List<T> list, final Function<T, R> op) {
-        /*
-         * Suggestion: consider Map.merge
-         */
-        return null;
+        final Map<R, Set<T>> map = new HashMap<>();
+        list.forEach(el -> {
+            final R key = op.apply(el);
+            if (map.containsKey(key)) {
+                map.get(key).add(el);
+            } else {
+                map.put(key, new HashSet<T>(Set.of(el)));
+            }
+        });
+        return map;
     }
 
     /**
@@ -96,12 +102,9 @@ public final class LambdaUtilities {
      *         by the supplier
      */
     public static <K, V> Map<K, V> fill(final Map<K, Optional<V>> map, final Supplier<V> def) {
-        /*
-         * Suggestion: consider Optional.orElse
-         *
-         * Keep in mind that a map can be iterated through its forEach method
-         */
-        return null;
+        final Map<K, V> m = new HashMap<>();
+        map.forEach((k, v) -> m.put(k, v.orElseGet(def::get)));
+        return m;
     }
 
     /**
